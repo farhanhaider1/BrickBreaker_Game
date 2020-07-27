fb7468e36f7d081f7f29cfe7932362d8b0b102af
farhan <fhaider085@gmail.com>
2020-07-27 11:58:05 -0400

    code to fill array

--- src/brickBreaker/MapGenerator.java
+++ src/brickBreaker/MapGenerator.java
@@ -1,4 +1,20 @@
 package brickBreaker;
 
 public class MapGenerator {
+    public int map[][];
+    public int brickWidth;
+    public int brickHeight;
+
+    public MapGenerator(int row, int column){
+        map = new int[row][column];
+        fillArray();
+    }
+
+    private void fillArray() {
+        for(int i = 0;i<map.length;i++){
+            for (int j = 0; j< map[0].length;j++){
+                map[i][j]=1;
+            }
+        }
+    }
 }
